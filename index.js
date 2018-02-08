c32d8fd7ee3579bb4498dd2b88a653109c9f369f
papachen <447334358@qq.com>
2018-02-08 22:44:41 +0800

    修改目标点坐标算法

--- index.js
+++ index.js
@@ -7,12 +7,17 @@ let redMax, redMin, greenMax, greenMin, blueMax, blueMin
 
 // 判断是否为背景色
 function isBg(r, g, b) {
-    return r <= redMax + 25 && r >= redMin - 25 && g <= greenMax + 25 && g >= greenMin - 25 && b <= blueMax + 25 && b >= blueMin - 25
+    return r <= redMax + 5 && r >= redMin - 5 && g <= greenMax + 5 && g >= greenMin - 5 && b <= blueMax + 5 && b >= blueMin - 5
 }
 
 // 小黑人最底部颜色范围
 function isBlack({r, g, b}) {
     return r < 65 && r > 45 && g < 70 && g > 45 && b > 85 && b < 115
+}
+
+// 判断两色是否相近
+function isSimilar(r1, g1, b1, r2, g2, b2) {
+    return Math.abs(r1 - r2) < 10 && Math.abs(g1 - g2) < 10 && Math.abs(b1 - b2) < 10
 }
 
 // 疑似小黑范围,用于目标点查找
@@ -59,23 +64,31 @@ async function getPosition() {
         console.log(`图片分辨率：宽${pixels.shape[0]}px,高${pixels.shape[1]}px`)
         let width = pixels.shape[0]
         let height = pixels.shape[1]
-        // 通过对左上、右上、左下、右下取点计算背景色取值范围(宽高范围分别缩小30个像素,以防止边界像素影响)
-        let {r: r1, g: g1, b: b1} = getRGB(pixels, 30, 30) // 左上
-        let {r: r2, g: g2, b: b2} = getRGB(pixels, width - 30, 30) // 右上
-        let {r: r3, g: g3, b: b3} = getRGB(pixels, 30, height - 30) // 左下
-        let {r: r4, g: g4, b: b4} = getRGB(pixels, width - 30, height - 30) // 右下
-        let {r: r5, g: g5, b: b5} = getRGB(pixels, width / 2, height - 10) // 下中
-        // 取背景色时,左下或右下可能会被跳台遮挡,因此再取下部中间色块,三点对比排除一个
-        if ((Math.abs(r5 - r4) < 10 && Math.abs(g5 - g4) < 10 && Math.abs(b5 - b4) < 10) || (Math.abs(r5 - r3) < 10 && Math.abs(g5 - g3) < 10 && Math.abs(b5 - b3) < 10)) {
-            if (Math.abs(r5 - r4) > 10 || Math.abs(g5 - g4) > 10 || Math.abs(b5 - b4) > 10) {
+        // 通过对左上、右上、左下、右下取点计算背景色取值范围(宽高范围分别缩小10个像素,以防止边界像素影响)
+        let {r: r1, g: g1, b: b1} = getRGB(pixels, 10, 10) // 左上
+        let {r: r2, g: g2, b: b2} = getRGB(pixels, width - 10, 10) // 右上
+        let {r: r3, g: g3, b: b3} = getRGB(pixels, 10, height - 10) // 左下
+        let {r: r4, g: g4, b: b4} = getRGB(pixels, width - 10, height - 10) // 右下
+        let {r: r5, g: g5, b: b5} = getRGB(pixels, width / 4, height - 10) // 下3
+        let {r: r6, g: g6, b: b6} = getRGB(pixels, width * 3 / 4, height - 10) // 下4
+        // 取背景色时,左下或右下可能会被跳台遮挡,因此下部取四个颜色
+        if (!isSimilar(r3, g3, b3, r4, g4, b4)) {
+            if (isSimilar(r3, g3, b3, r5, g5, b5)) {
                 r4 = r5
                 g4 = g5
                 b4 = b5
-            }
-            if (Math.abs(r5 - r3) > 10 || Math.abs(g5 - g3) > 10 || Math.abs(b5 - b3) > 10) {
+            } else if (isSimilar(r3, g3, b3, r6, g6, b6)) {
+                r4 = r6
+                g4 = g6
+                b4 = b6
+            } else if (isSimilar(r4, g4, b4, r5, g5, b5)) {
                 r3 = r5
                 g3 = g5
                 b3 = b5
+            } else {
+                r3 = r6
+                g3 = g6
+                b3 = b6
             }
         }
         redMax = Math.max(r1, r2, r3, r4)
@@ -92,7 +105,7 @@ async function getPosition() {
             for (let j = 100; j < width - 100; j++) {
                 let px = getRGB(pixels, j, i)
                 // 当此色块不属于背景色时,开始分析
-                if (!(isBg(px.r, px.g, px.b))) {
+                if (!isBg(px.r, px.g, px.b)) {
                     // 当blackPoint不存在或扫描到的色块y值大于已记录值,则记录该点
                     if ((!blackPoint || blackPoint.y < i) && isBlack(px)) {
                         blackPoint = {
@@ -104,37 +117,28 @@ async function getPosition() {
             }
         }
         if (blackPoint) {
-            blackPoint.x += 10 // 修正值
+            // 小黑点坐标修正值
+            blackPoint.x += 7
+            blackPoint.y -= 15
             console.log(`小黑坐标为：x:${blackPoint.x},y:${blackPoint.y}`)
             // 如果找到小黑点，则开始分析目标点坐标
             console.log(`正在分析目标点坐标...`)
-            // 记录目标平台的顶点坐标和最右点坐标
-            // 取目标平台的最顶点和最右点(因为游戏中阴影方向为左上,因此取最右点),最右点x坐标与顶点y坐标即为目的坐标
+            // 记录目标平台的顶点坐标,然后根据顶点坐标估算出下方的目标点坐标
             // 此算法非最优,计算结果不够精确,待优化
-            let topPoint, rightPoint
+            let target
             // 从左上角开始,由上向下,由左到右依次扫描
             for (let i = 100; i < width - 100; i++) {
                 for (let j = 350; j < height - 350; j++) {
                     let px = getRGB(pixels, i, j)
                     // 当此色块不属于背景色且非疑似小黑时,记录该点
                     if (!isBg(px.r, px.g, px.b) && !isBlack2(px)) {
-                        // 顶点值取值
-                        // 当扫描到的符合条件的色块y值小于已记录的值,则用新值覆盖旧值
-                        if (!topPoint || topPoint.y > j) {
-                            topPoint = {
+                        if (!target || target.y > j) {
+                            target = {
                                 x: i,
                                 y: j
                             }
+                            break
                         }
-                        // 这里增加最右点取点时的判断,由于起点平台的阴影会映射到目标平台的区域
-                        // 因此如果下个取点y轴方向下落距离超过50px,则判定此点无效
-                        if (!rightPoint || j - topPoint.y < 50) {
-                            rightPoint = {
-                                x: i,
-                                y: j
-                            }
-                        }
-                        break
                     }
                     // 如果扫描到第一个疑似小黑的点,则结束当前y坐标循环
                     if (isBlack2(px)) {
@@ -142,10 +146,14 @@ async function getPosition() {
                     }
                 }
             }
-            if (topPoint && rightPoint) {
-                let target = {
-                    x: topPoint.x,
-                    y: rightPoint.y
+            if (target) {
+                // 根据目标点与小黑的距离对目的坐标进行修正
+                if (target.x > blackPoint.x) {
+                    target.y += 25
+                    target.x -= 5
+                } else {
+                    target.y += 25
+                    target.x += 5
                 }
                 console.log(`目标点坐标为：x:${target.x},y:${target.y}`)
                 return {target, blackPoint}
@@ -175,13 +183,13 @@ function auto() {
             if (res) {
                 // 距离时间系数：按压时间 = 两点距离 * 系数
                 // 系数越大,跳的越远
-                const k = 1.309
+                const k = 1.3082
                 // 计算起点与目标点的距离
                 const instance = Math.sqrt(Math.pow(res.target.x - res.blackPoint.x, 2) + Math.pow(res.target.y - res.blackPoint.y, 2))
                 const press_time = instance * k
                 // 随机点按压,避开微信的防外挂检测
                 console.log('开始起跳...')
-                jump(random(400, 800), random(800, 1600), parseInt(press_time))
+                jump(random(500, 700), random(700, 1000), parseInt(press_time))
                 // 随机停顿,也是为了避开微信的防外挂检测
                 console.log('等待中...')
                 setTimeout(auto, random(500, 1200))
